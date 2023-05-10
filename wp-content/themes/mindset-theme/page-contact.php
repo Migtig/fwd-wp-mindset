08e8e8d37077f9e73199dfa29b96bc37f4ed716a
Connor <cfroese4@my.bcit.ca>
2023-05-10 10:34:15 -0700

    day 6 class work stuff

---
+++ wp-content/themes/mindset-theme/page-contact.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package FWD_Starter_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		<?php
+		if( function_exists( 'get_field' ) ) {
+			if ( get_field('address') ) {
+				the_field('address');
+			}
+			if ( get_field('email') ) {
+				$email = get_field('email');
+				$mailto = 'mailto:' . $email;
+				?>
+				<p><a href="<?php echo esc_url( $mailto ); ?> "><?php echo esc_html( $email ); ?></a></p>
+				<?php
+			}
+		}
+		?>
+	</main><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
